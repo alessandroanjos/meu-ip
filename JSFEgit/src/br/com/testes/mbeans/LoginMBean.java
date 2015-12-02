308b09f364134ff71cc3cf2995cf3de5ec71c385
alessandroanjos <sandro.adsc@gmail.com>
2015-12-02 18:10:58 -0200

    Add the new login bean.

---
+++ JSFEgit/src/br/com/testes/mbeans/LoginMBean.java
@@ -0,0 +1,17 @@
+package br.com.testes.mbeans;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class LoginMBean {
+
+	private String email;
+	private String senha;
+	
+	public LoginMBean() {
+	}
+	
+	public String login(){
+		return null;
+	}
+}
